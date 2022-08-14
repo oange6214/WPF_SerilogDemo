eeabe75bf83e41dfdc2d8bf46a94461aeea4a4b6
jed <oange6214@gmail.com>
2022-08-14 22:30:39 +0800

    Add Serilog & Host Demo

---
+++ LoggingDemo/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Input;
+
+namespace LoggingDemo.ViewModels
+{
+    public class MainViewModel
+    {
+        public ICommand MakeSandwichCommand { get; }
+
+        public MainViewModel(ICommand makeSandwichCommand)
+        {
+            MakeSandwichCommand = makeSandwichCommand;
+        }
+    }
+}
